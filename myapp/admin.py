975d1fc1547047acf5802e3ed4ff1dac259ee2ca
brucebett <bett6448@gmail.com>
2024-11-20 01:02:18 +0300

    teu4 commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Internship)
+
+class InternshipAdmin(admin.ModelAdmin):
+    list_display = (
+    'fullname',
+    'usn',
+    'email',
+    'college_name',
+    'offer_status',
+    'start_date',
+    'end_date',
+    'project_report',
+    'timeStamp')
+    search_fields=('fullname','usn','email')
+    list_filter=['college_name','project_report','offer_status']
+
+admin.site.register(Internship,InternshipAdmin)
